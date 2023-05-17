7e633ccbb21c565f6af86a88706572da7e9d9210
Mark Anthony <manthonyamaro@yahoo.com>
2023-05-16 20:24:25 -0500

    removed past,present,future from html and added hours above 11 to 17 to set upa way to get all blocks a color. Created a function that each time block has a selected id that pulls from the local storage and saves the text while checking for empty values in the storage data

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ $(".saveBtn").on("click", function () {
     var eventText = $(this).siblings(".description").val();
     localStorage.setItem(time, eventText);
   });
+
+ 
+  // when information is submitted, this function will save and persist data
+$(".time-block").each(function () {
+  var time = $(this).attr("id");
+  var savedEvent = localStorage.getItem(time);
+  if (savedEvent !== null) {
+    $(this).children(".description").val(savedEvent);
+  }
+});
